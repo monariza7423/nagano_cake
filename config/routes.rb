ae806f5e0a705e2309d1e53d580500e32ffb0ff0
EC2 Default User <ec2-user@ip-172-31-11-185.ec2.internal>
2022-07-17 07:54:45 +0000

    model作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  devise_for :customers
+  devise_for :admins
+  get 'addresses/index'
+  get 'addresses/edit'
+  get 'orders/new'
+  get 'orders/index'
+  get 'orders/show'
+  get 'orders/confirm'
+  get 'orders/complete'
+  get 'cart_items/index'
+  get 'customers/show'
+  get 'customers/edit'
+  get 'customers/confirm'
+  get 'customers/withdraw'
+  get 'items/index'
+  get 'items/show'
+  devise_for :users
+  root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
