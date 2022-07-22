39e103e6d1d5b05062845c4e204fbba9111afb61
EC2 Default User <ec2-user@ip-172-31-11-185.ec2.internal>
2022-07-22 11:59:24 +0000

    商品ページ

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -14,6 +14,21 @@ class Admin::ItemsController < ApplicationController
     @items = Item.all
   end
   
+  def show
+    @item = Item.find(params[:id])
+  end
+  
+  def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+  end
+  
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item.id)
+  end
+  
   private
   def item_params
     params.require(:item).permit(:name, :introduction, :price, :image, :is_active, :genre_id)
