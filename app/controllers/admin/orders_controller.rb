a8967e37f66b15190ebb13431f5782d70e3a71bd
EC2 Default User <ec2-user@ip-172-31-11-185.ec2.internal>
2022-08-10 12:08:40 +0000

    注文履歴上部分

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,13 @@
 class Admin::OrdersController < ApplicationController
+  def show
+    @order = Order.find(params[:id])
+  end
+  
+  def update
+  end
+  
+  private
+  def order_params
+    params.require(:order).permit(:invoice_postalcode, :invoice_address, :pay_method, :invoice_name, :postage, :billing, :status)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
     resources :genres, only:[:new, :create, :index, :edit, :update]
     resources :items, only:[:new, :create, :index, :show, :edit, :update]
     resources :customers, only:[:index, :show, :edit, :update]
-    resources :order, only:[:show, :update]
+    resources :orders, only:[:show, :update]
     
   end
   
